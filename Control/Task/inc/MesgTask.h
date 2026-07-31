fe8104838f44830b5f1d43db5432148a766d88d3
Lajgit <3243570032@qq.com>
2026-07-31 15:34:15 +0800

    恢复固件版本并标记K2修复版本

--- Control/Task/inc/MesgTask.h
+++ Control/Task/inc/MesgTask.h
@@ -8,9 +8,9 @@
 #define Android_USART USART1
 
 #define VERSION_MAJOR 1U
-#define VERSION_MINOR 0U
+#define VERSION_MINOR 3U
 #define VERSION_PATCH 0U
-#define VERSION_BUILD 0U
+#define VERSION_BUILD 1U
 #define VERSION ((VERSION_MAJOR << 24) | (VERSION_MINOR << 16) | \
                  (VERSION_PATCH << 8) | VERSION_BUILD)
 
