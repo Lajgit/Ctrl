4f739d4ce01d1148d1c62298e709f0593940e6e7
Lajgit <3243570032@qq.com>
2026-08-04 19:20:40 +0800

    控制板版本升级到2.2.0.1

--- Control/Task/inc/MesgTask.h
+++ Control/Task/inc/MesgTask.h
@@ -10,7 +10,7 @@
 #define VERSION_MAJOR 2U
 #define VERSION_MINOR 2U
 #define VERSION_PATCH 0U
-#define VERSION_BUILD 0U
+#define VERSION_BUILD 1U
 #define VERSION ((VERSION_MAJOR << 24) | (VERSION_MINOR << 16) | \
                  (VERSION_PATCH << 8) | VERSION_BUILD)
 
